bbfbd2ec43021e805a161e77a5adc18f3e77bc06
rishigits <rishigits@users.noreply.github.com>
2025-09-05 15:46:08 -0400

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -5,6 +5,7 @@ version=${arr[1]}
 trap 0 1 2 ERR
 # Ensure sudo is installed along with utilities to build rpm and deb packages
 apt-get update && apt-get install sudo ruby ruby-dev rpm -y
+sudo gem install fpm
 bash /tmp/linux-on-ibm-z-scripts/Terraform/${version}/build_terraform.sh -y
 tar cvfz terraform-${version}-linux-s390x.tar.gz -C $PWD/bin terraform -C $PWD/src/github.com/hashicorp/terraform LICENSE
 fpm -s tar -t rpm --prefix /usr/bin -n terraform -v ${version} terraform-${version}-linux-s390x.tar.gz
